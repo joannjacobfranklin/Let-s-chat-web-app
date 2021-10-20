df65552f22dc6144f37088ff2a25a52f34666d40
joannjacobfranklin <78270607+joannjacobfranklin@users.noreply.github.com>
2021-10-20 22:11:14 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+    apiKey : "AIzaSyAUzoZtC8nil5erIXq9u80P8MxOT6rWUlc" , 
+    authDomain: "chat-app-9b39e.firebaseapp.com",
+    databaseURL: "https://chat-app-9b39e-default-rtdb.firebaseio.com",
+    projectId: "chat-app-9b39e",
+    storageBucket: "chat-app-9b39e.appspot.com",
+    messagingSenderId: "555486925206",
+    appId: "1:555486925206:web:286044fbd3b7c666fd4db7",
+    measurementId: "G-M8VF0Y2E1Z"
+  };
+  
+  // Initialize Firebase
+  var app = initializeApp(firebaseConfig);
+  var analytics = getAnalytics(app);
